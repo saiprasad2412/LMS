4b238dfd265b72c618eb4d44c15b784c0a3a6a6e
Saiprasad Mane <95936681+saiprasad2412@users.noreply.github.com>
2025-01-30 21:59:05 +0530

    setup with shadcn css

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+   
+      <Button>Click me</Button>
+   
+  )
+}
+
+export default App
